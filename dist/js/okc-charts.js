2d85624cbb614832115bf368139d88da7418decb
Ruby Walker <avw7@pitt.edu>
2017-02-09 14:37:47 -0500

    created columns and added second chart

--- dist/js/okc-charts.js
+++ dist/js/okc-charts.js
@@ -1,6 +1,8 @@
 google.charts.load("current", {packages:["corechart"]});
-google.charts.setOnLoadCallback(drawChart);
-function drawChart() {
+google.charts.setOnLoadCallback(drawAgeNeverChart);
+google.charts.setOnLoadCallback(drawAgeSometimesChart);
+google.charts.setOnLoadCallback(drawAgeNeverChart);
+function drawAgeNeverChart() {
   var data = google.visualization.arrayToDataTable([
     ['Age Bracket', 'User'],
     ['18-21',     1860],
@@ -13,7 +15,62 @@ function drawChart() {
 
   var options = {
     pieHole: 0.4,
-    backgroundColor: '#780074',
+    backgroundColor: 'transparent',
+    legend: {
+      position: 'none'
+    },
+    titleTextStyle: { color: 'transparent',
+  fontSize: 20,
+  fontAlign: 'center'}
+};
+
+  var chart = new google.visualization.PieChart(document.getElementById('AgeNever'));
+chart.draw(data, options);
+
+}
+
+  function drawAgeSometimesChart() {
+    var data = google.visualization.arrayToDataTable([
+      ['Age Bracket', 'User'],
+      ['18-21',     2],
+      ['26-30',     3],
+      ['31-40',     6],
+      ['41-50',     2],
+      ['51-60',     5],
+      ['61-70',     2]
+    ]);
+
+    var options = {
+      pieHole: 0.4,
+      backgroundColor: 'transparent',
+      legend: {
+        position: 'none'
+      },
+      titleTextStyle: { color: 'transparent',
+    fontSize: 20,
+    fontAlign: 'center'}
+    };
+
+
+      var chart = new google.visualization.PieChart(document.getElementById('AgeSometimes'));
+    chart.draw(data, options);
+
+}
+
+function drawAgeOftenChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Age Bracket', 'User'],
+    ['18-21',     2],
+    ['26-30',     3],
+    ['31-40',     6],
+    ['41-50',     2],
+    ['51-60',     5],
+    ['61-70',     2]
+  ]);
+
+  var options = {
+    pieHole: 0.4,
+    backgroundColor: 'transparent',
     legend: {
       position: 'none'
     },
@@ -22,6 +79,8 @@ function drawChart() {
   fontAlign: 'center'}
   };
 
-  var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+
+    var chart = new google.visualization.PieChart(document.getElementById('AgeOften'));
   chart.draw(data, options);
+
 }
