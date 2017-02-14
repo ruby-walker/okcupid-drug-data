3c594bdd21fcd95406224be8b22c875197d6c0f7
Ruby Walker <avw7@pitt.edu>
2017-02-14 10:41:00 -0500

    added data for race

--- dist/js/okc-charts.js
+++ dist/js/okc-charts.js
@@ -19,7 +19,7 @@ google.charts.setOnLoadCallback(drawIndustrySometimesChart);
 google.charts.setOnLoadCallback(drawIndustryOftenChart);
 function drawAgeNeverChart() {
   var data = google.visualization.arrayToDataTable([
-    ['Age Bracket', 'User'],
+    ['Age Bracket', 'Users'],
     ['18-21',     1860],
     ['22-25',     6453],
     ['26-30',     10551],
@@ -46,7 +46,7 @@ chart.draw(data, options);
 
   function drawAgeSometimesChart() {
     var data = google.visualization.arrayToDataTable([
-      ['Age Bracket', 'User'],
+      ['Age Bracket', 'Users'],
       ['18-21',     767],
       ['22-25',     1979],
       ['26-30',     2375],
@@ -74,7 +74,7 @@ chart.draw(data, options);
 
 function drawAgeOftenChart() {
   var data = google.visualization.arrayToDataTable([
-    ['Age Bracket', 'User'],
+    ['Age Bracket', 'Users'],
     ['18-21',     103],
     ['22-25',     132],
     ['26-30',     91],
@@ -103,13 +103,17 @@ function drawAgeOftenChart() {
 
 function drawRaceNeverChart() {
   var data = google.visualization.arrayToDataTable([
-    ['Age Bracket', 'User'],
-    ['18-21',     1860],
-    ['22-25',     6453],
-    ['26-30',     10551],
-    ['31-40',     11232],
-    ['41-50',     4767],
-    ['51+',     2860]
+    ['Race', 'Users'],
+    ['White',             9794],
+    ['Black',             386],
+    ['Black / White',     134],
+    ['Hispanic',          674],
+    ['Asian',             762],
+    ['Indian',            108],
+    ['Middle Eastern',    86],
+    ['Pacific Islander',  198],
+    ['Native American',   18],
+    ['Other',             1984]
   ]);
 
   var options = {
@@ -130,13 +134,17 @@ chart.draw(data, options);
 
   function drawRaceSometimesChart() {
     var data = google.visualization.arrayToDataTable([
-      ['Age Bracket', 'User'],
-      ['18-21',     767],
-      ['22-25',     1979],
-      ['26-30',     2375],
-      ['31-40',     1904],
-      ['41-50',     556],
-      ['51+',     250],
+      ['Age Bracket', 'Users'],
+      ['White',             4898],
+      ['Black',             195],
+      ['Black / White',     66],
+      ['Hispanic',          338],
+      ['Asian',             381],
+      ['Indian',            55],
+      ['Middle Eastern',    43],
+      ['Pacific Islander',  100],
+      ['Native American',   10],
+      ['Other',             992]
     ]);
 
     var options = {
@@ -158,13 +166,17 @@ chart.draw(data, options);
 
 function drawRaceOftenChart() {
   var data = google.visualization.arrayToDataTable([
-    ['Age Bracket', 'User'],
-    ['18-21',     103],
-    ['22-25',     132],
-    ['26-30',     91],
-    ['31-40',     59],
-    ['41-50',     13],
-    ['51+',       5]
+    ['Age Bracket', 'Users'],
+    ['White',             220],
+    ['Black',             23],
+    ['Black / White',     7],
+    ['Hispanic',          45],
+    ['Asian',             13],
+    ['Indian',            1],
+    ['Middle Eastern',    3],
+    ['Pacific Islander',  5],
+    ['Native American',   2],
+    ['Other',             66]
   ]);
 
 
@@ -187,7 +199,7 @@ function drawRaceOftenChart() {
 
 function drawReligionNeverChart() {
   var data = google.visualization.arrayToDataTable([
-    ['Age Bracket', 'User'],
+    ['Age Bracket', 'Users'],
     ['18-21',     1860],
     ['22-25',     6453],
     ['26-30',     10551],
@@ -214,7 +226,7 @@ chart.draw(data, options);
 
   function drawReligionSometimesChart() {
     var data = google.visualization.arrayToDataTable([
-      ['Age Bracket', 'User'],
+      ['Age Bracket', 'Users'],
       ['18-21',     767],
       ['22-25',     1979],
       ['26-30',     2375],
@@ -242,7 +254,7 @@ chart.draw(data, options);
 
 function drawReligionOftenChart() {
   var data = google.visualization.arrayToDataTable([
-    ['Age Bracket', 'User'],
+    ['Age Bracket', 'Users'],
     ['18-21',     103],
     ['22-25',     132],
     ['26-30',     91],
@@ -271,7 +283,7 @@ function drawReligionOftenChart() {
 
 function drawDrinkingNeverChart() {
   var data = google.visualization.arrayToDataTable([
-    ['Age Bracket', 'User'],
+    ['Age Bracket', 'Users'],
     ['18-21',     1860],
     ['22-25',     6453],
     ['26-30',     10551],
@@ -298,7 +310,7 @@ chart.draw(data, options);
 
   function drawDrinkingSometimesChart() {
     var data = google.visualization.arrayToDataTable([
-      ['Age Bracket', 'User'],
+      ['Age Bracket', 'Users'],
       ['18-21',     767],
       ['22-25',     1979],
       ['26-30',     2375],
@@ -326,7 +338,7 @@ chart.draw(data, options);
 
 function drawDrinkingOftenChart() {
   var data = google.visualization.arrayToDataTable([
-    ['Age Bracket', 'User'],
+    ['Age Bracket', 'Users'],
     ['18-21',     103],
     ['22-25',     132],
     ['26-30',     91],
@@ -356,7 +368,7 @@ function drawDrinkingOftenChart() {
 
 function drawOrientationNeverChart() {
   var data = google.visualization.arrayToDataTable([
-    ['Age Bracket', 'User'],
+    ['Age Bracket', 'Users'],
     ['18-21',     1860],
     ['22-25',     6453],
     ['26-30',     10551],
@@ -383,7 +395,7 @@ chart.draw(data, options);
 
   function drawOrientationSometimesChart() {
     var data = google.visualization.arrayToDataTable([
-      ['Age Bracket', 'User'],
+      ['Age Bracket', 'Users'],
       ['18-21',     767],
       ['22-25',     1979],
       ['26-30',     2375],
@@ -411,7 +423,7 @@ chart.draw(data, options);
 
 function drawOrientationOftenChart() {
   var data = google.visualization.arrayToDataTable([
-    ['Age Bracket', 'User'],
+    ['Age Bracket', 'Users'],
     ['18-21',     103],
     ['22-25',     132],
     ['26-30',     91],
@@ -441,7 +453,7 @@ function drawOrientationOftenChart() {
 
 function drawIndustryNeverChart() {
   var data = google.visualization.arrayToDataTable([
-    ['Age Bracket', 'User'],
+    ['Age Bracket', 'Users'],
     ['18-21',     1860],
     ['22-25',     6453],
     ['26-30',     10551],
@@ -468,7 +480,7 @@ chart.draw(data, options);
 
   function drawIndustrySometimesChart() {
     var data = google.visualization.arrayToDataTable([
-      ['Age Bracket', 'User'],
+      ['Age Bracket', 'Users'],
       ['18-21',     767],
       ['22-25',     1979],
       ['26-30',     2375],
@@ -496,7 +508,7 @@ chart.draw(data, options);
 
 function drawIndustryOftenChart() {
   var data = google.visualization.arrayToDataTable([
-    ['Age Bracket', 'User'],
+    ['Age Bracket', 'Users'],
     ['18-21',     103],
     ['22-25',     132],
     ['26-30',     91],
